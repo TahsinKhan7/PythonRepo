2ff7cc14852d6acb6bbb064afd3123cc94d25f99
Tahsin Khan <tahsin_k@live.co.uk>
2019-04-17 14:28:03 +0100

    added for loops

---
+++ for_loops.py
@@ -0,0 +1,10 @@
+# For loops
+# for place_holder in list_object
+# excecute this block of code
+ids_list = ['fiat', 'Lambo', 'Russian car', 'potato']
+
+counter = 0
+for id in ids_list:
+    #print(ids_list)
+    print(counter+1, '-', id)
+    counter += 1
